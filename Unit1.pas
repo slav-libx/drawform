82f574f145701c0f537ba300907f88c5c9d163ab
slav <my_990@bk.ru>
2019-11-14 17:11:17 +0300

    1111

--- Unit1.pas
+++ Unit1.pas
@@ -3,10 +3,12 @@ unit Unit1;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls, System.Math.Vectors, FMX.Ani,
-  FMX.Objects, FMX.Utils;
+  System.SysUtils, System.Types, System.UITypes, System.UIConsts, FMX.Objects,
+  FMX.StdCtrls, System.Classes, FMX.Types, FMX.Controls,
+  FMX.Controls.Presentation, System.Variants,
+  FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  System.Math.Vectors, FMX.Ani,
+  FMX.Utils;
 
 type
   TForm1 = class(TForm)
@@ -100,10 +102,113 @@ begin
 end;
 
 procedure TForm1.PaintRects(const UpdateRects: array of TRectF);
+var
+  I, J: Integer;
+  R: TRectF;
+  CallOnPaint, AllowPaint: Boolean;
+  Control: TControl;
 begin
-//  if FShowAnimation.Running or (Canvas.Offset.X=0) then
-//  Canvas.ClearRect(R,);
   inherited;
+  Exit;
+
+  if Canvas = nil then
+    Exit;
+
+//  if not FDrawing then
+//  begin
+
+//    AddUpdateRects(UpdateRects);
+//    PrepareForPaint;
+//
+//    FDrawing := True;
+//    try
+
+       R:=RectF(100,100,150,150);
+
+      if Canvas.BeginScene(@UpdateRects, ContextHandle) then
+      try
+
+        Canvas.Clear(claNull);
+        Canvas.Fill.Color:=claRed;
+        Canvas.SetMatrix(TMatrix.CreateRotation(0.5));
+
+        Canvas.FillRect(R,5,5,AllCorners,1);
+
+      finally
+        Canvas.EndScene;
+      end;
+
+//        PaintBackground;
+//
+//        CallOnPaint := False;
+//
+//        if Controls <> nil then
+//          for I := 0 to Controls.Count - 1 do
+//            if FControls[I].Visible or FControls[I].ShouldTestMouseHits then
+//            begin
+//              Control := FControls[I];
+//              if Control = FResourceLink then
+//              begin
+//                if Self.Transparency then
+//                  Continue;
+//                if Self.Fill.Kind <> TBrushKind.None then
+//                  Continue;
+//                if (Self.Fill.Kind = TBrushKind.Solid) and (Self.Fill.Color <> Fill.DefaultColor) then
+//                  Continue;
+//              end;
+//              if Control.UpdateRect.IsEmpty then
+//                Continue;
+//              AllowPaint := False;
+//              if Control.InPaintTo then
+//                AllowPaint := True;
+//              if not AllowPaint then
+//              begin
+//                R := UnionRect(Control.ChildrenRect, Control.UpdateRect);
+//                for J := 0 to High(FUpdateRects) do
+//                  if IntersectRect(FUpdateRects[J], R) then
+//                  begin
+//                    AllowPaint := True;
+//                    Break;
+//                  end;
+//              end;
+//              if AllowPaint then
+//                TOpenControl(Control).PaintInternal;
+//
+//              if Control = FResourceLink then
+//              begin
+//                Canvas.SetMatrix(TMatrix.Identity);
+//                DoPaint(Self.Canvas, ClientRect);
+//
+//                {$IFDEF MSWINDOWS}
+//                if (csDesigning in ComponentState) and (Designer <> nil) then
+//                begin
+//                  Canvas.SetMatrix(TMatrix.Identity);
+//                  Designer.PaintGrid;
+//                end;
+//                {$ENDIF}
+//
+//                CallOnPaint := True;
+//              end;
+//            end;
+//
+//          if not CallOnPaint then
+//          begin
+//            Canvas.SetMatrix(TMatrix.Identity);
+//            DoPaint(Canvas, ClientRect);
+//          end;
+//
+//          {$IFDEF MSWINDOWS}
+//          if (csDesigning in ComponentState) and (Designer <> nil) then
+//            Designer.Decorate(nil);
+//          {$ENDIF}
+//      finally
+//        Canvas.EndScene;
+//      end;
+//    finally
+//      SetLength(FUpdateRects, 0);
+//      FDrawing := False;
+//    end;
+//  end;
 end;
 
 end.
